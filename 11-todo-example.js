e3ff45d0748d6be38276caae529266844aa4f5b1
joelshajan1551 <joelshajan0000@gmail.com>
2023-05-09 19:16:18 +0530

    added todo exercise 2

---
+++ 11-todo-example.js
@@ -0,0 +1,46 @@
+const todoList = [
+  {
+    name: "make dinner",
+    dueDate: "04-05-2023",
+  },
+  {
+    name: "wash dishes",
+    dueDate: "20-12-2202",
+  },
+];
+
+function renderTodoList() {
+  let todoListHtml = "";
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const { name, dueDate } = todoObject;
+
+    const html = `<p>${name} ${dueDate}<button onclick="
+    todoList.splice(${i}, 1)
+    renderTodoList();";
+    
+    >Delete</button></p>`;
+    todoListHtml += html;
+  }
+  // console.log(todoListHtml);
+
+  document.querySelector(".js-todo-list").innerHTML = todoListHtml;
+}
+
+function addTodo() {
+  const inputElement = document.querySelector(".js-todoNameInput");
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector(".js-due-date-input");
+
+  const dueDate = dateInputElement.value;
+  // console.log(name);
+
+  todoList.push(name);
+  // console.log(todoList);
+
+  inputElement.value = "";
+
+  renderTodoList();
+}
